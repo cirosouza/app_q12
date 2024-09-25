20b4317fb7252c3d4183aa7f22dd7836bf43f2af
cirosouza <ciro_lut@hotmail.com>
2024-09-25 14:00:21 -0300

    First commit do projeto

---
+++ src/main/java/org/javaexercises/kubernetesactionsservice/KubernetesActionsServiceApplication.java
@@ -0,0 +1,13 @@
+package org.javaexercises.kubernetesactionsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesActionsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubernetesActionsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/javaexercises/kubernetesactionsservice/KubernetesActionsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javaexercises.kubernetesactionsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesActionsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
